1df4f10372bf848474564fac00eeff68de6d1368
Fatih Doğan <fatih@fatihdogan.dev>
2019-09-29 00:32:18 +0300

    jfoenix

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.fthdgn.app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
     opens com.fthdgn.app to javafx.graphics;
     opens com.fthdgn.app.controller to javafx.fxml;
 }
